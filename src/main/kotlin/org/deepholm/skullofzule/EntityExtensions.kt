dea200c138a5ea05b506569e09e75c04cd65738f
Jacob Queen <jacobsq@protonmail.com>
2020-11-09 08:48:24 -0700

    ,.

--- src/main/kotlin/org/deepholm/skullofzule/EntityExtensions.kt
+++ src/main/kotlin/org/deepholm/skullofzule/EntityExtensions.kt
@@ -20,6 +20,7 @@ import org.hexworks.cobalt.datatypes.extensions.orElseThrow
 import org.hexworks.cobalt.datatypes.extensions.map
 import org.hexworks.zircon.api.data.Tile
 import kotlin.reflect.KClass
+import kotlin.reflect.full.isSubclassOf
 import kotlin.reflect.full.isSuperclassOf
 
 var AnyGameEntity.position
@@ -67,4 +68,10 @@ fun GameEntity<Combatant>.whenHasNoHealthLeft(fn: () -> Unit) {
 
 inline fun <reified T : EntityType> Iterable<AnyGameEntity>.filterType(): List<Entity<T, GameContext>> {
     return filter {T::class.isSuperclassOf(it.type::class)}.toList() as List<Entity<T, GameContext>>
+}
+
+inline fun <reified T : EntityType> AnyGameEntity.whenTypeIs(fn: (Entity<T, GameContext>) -> Unit) {
+    if (this.type::class.isSubclassOf(T::class)) {
+        fn(this as Entity<T, GameContext>)
+    }
 }
---
+++ src/main/kotlin/org/deepholm/skullofzule/systems/EnergyExpender.kt
@@ -0,0 +1,48 @@
+package org.deepholm.skullofzule.systems
+
+import org.deepholm.skullofzule.GameContext
+import org.deepholm.skullofzule.attributes.EnergyLevel
+import org.deepholm.skullofzule.attributes.types.EnergyUser
+import org.deepholm.skullofzule.attributes.types.energyLevel
+import org.deepholm.skullofzule.commands.Destroy
+import org.deepholm.skullofzule.commands.ExpendEnergy
+import org.deepholm.skullofzule.extensions.GameCommand
+import org.deepholm.skullofzule.extensions.GameEntity
+import org.deepholm.skullofzule.whenTypeIs
+import org.hexworks.amethyst.api.Consumed
+import org.hexworks.amethyst.api.Response
+import org.hexworks.amethyst.api.base.BaseActor
+import org.hexworks.amethyst.api.entity.EntityType
+
+object EnergyExpender : BaseActor<GameContext>(EnergyLevel::class) {
+
+    override fun executeCommand(command: GameCommand<out EntityType>): Response {
+        return command.responseWhenCommandIs(ExpendEnergy::class) { (context, entity, energy) ->
+            entity.energyLevel.currentEnergy -= energy
+            checkStarvation(context, entity, entity.energyLevel)
+            Consumed
+        }
+    }
+
+    override fun update(entity: GameEntity<EntityType>, context: GameContext): Boolean {
+        entity.whenTypeIs<EnergyUser> {
+            entity.executeCommand(ExpendEnergy(
+                    context = context,
+                    source = it,
+                    energy = 2))
+        }
+        return true
+    }
+
+    private fun checkStarvation(context: GameContext,
+                                entity: GameEntity<EntityType>,
+                                energyLevel: EnergyLevel) {
+        if (energyLevel.currentEnergy <= 0) {
+            entity.executeCommand(Destroy(
+                    context = context,
+                    source = entity,
+                    target = entity,
+                    cause = "starvation"))
+        }
+    }
+}
